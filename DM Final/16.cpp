e654a0262e4e6e95a6207ab8124812ad3b59d5f8
Md. Sadekur Rahman Sahad <sahad.cse8.bu@gmail.com>
2023-10-17 22:16:29 +0600

    Update 16.cpp

--- DM Final/16.cpp
+++ DM Final/16.cpp
@@ -1,77 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-// Define the structure of a BST node
-struct TreeNode
-{
-    int data;
-    struct TreeNode *left;
-    struct TreeNode *right;
+struct Node {
+    int value;
+    Node *left;
+    Node *right;
+    
+    Node(int item) {
+        value = item;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-// Function to create a new BST node
-struct TreeNode *createNode(int item)
-{
-    struct TreeNode *newNode = (struct TreeNode *)malloc(sizeof(struct TreeNode));
-    newNode->data = item;
-    newNode->left = newNode->right = NULL;
-    return newNode;
+Node *insertIntoBST(Node *rootNode, int item) {
+    if (rootNode == nullptr) return new Node(item);
+    if (item < rootNode -> value) rootNode -> left = insertIntoBST(rootNode -> left, item);
+    else if (item >= rootNode -> value) rootNode -> right = insertIntoBST(rootNode -> right, item);
+    return rootNode;
 }
 
-// Function to insert a new node into the BST
-struct TreeNode *insert(struct TreeNode *root, int item)
-{
-    // If the tree is empty, return a new node
-    if (root == NULL)
-    {
-        return createNode(item);
-    }
-
-    // Otherwise, recur down the tree
-    if (item < root->data)
-    {
-        root->left = insert(root->left, item);
-    }
-    else if (item > root->data)
-    {
-        root->right = insert(root->right, item);
-    }
-
-    // Return the (unchanged) node pointer
-    return root;
+void printInOrder(Node *rootNode) {
+    if (rootNode == nullptr) return;
+    printInOrder(rootNode -> left);
+    cout << rootNode -> value << " ";
+    printInOrder(rootNode -> right);
 }
 
-// Function to perform an inorder traversal of the BST
-void inorderTraversal(struct TreeNode *root)
-{
-    if (root != NULL)
-    {
-        inorderTraversal(root->left);
-        printf("%d ", root->data);
-        inorderTraversal(root->right);
+int main() {
+    int n; 
+    scanf("%d", &n);
+    int items[n];
+    for (int i = 0; i < n; i++) scanf("%d", &items[i]);
+    
+    Node *rootNode = nullptr;
+    for (int i = 0; i < n; i++) {
+        rootNode = insertIntoBST(rootNode, items[i]);
     }
-}
-
-// Function to construct a BST from a list of items
-struct TreeNode *constructBST(int items[], int n)
-{
-    struct TreeNode *root = NULL;
-    for (int i = 0; i < n; i++)
-    {
-        root = insert(root, items[i]);
-    }
-    return root;
-}
-
-int main()
-{
-    int items[] = {50, 30, 70, 20, 40, 60, 80};
-    int n = sizeof(items) / sizeof(items[0]);
-
-    struct TreeNode *root = constructBST(items, n);
-
-    printf("Inorder traversal of the constructed BST:\n");
-    inorderTraversal(root);
-
+    
+    printf("In-order traversal of the constructed BST: ");
+    printInOrder(rootNode);
+    printf("\n");
+    
     return 0;
 }
