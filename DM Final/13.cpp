d8b6dadde4f518acec1a5072313164659b064f87
Md. Sadekur Rahman Sahad <sahad.cse8.bu@gmail.com>
2023-10-17 22:10:56 +0600

    13.cpp

--- DM Final/13.cpp
+++ DM Final/13.cpp
@@ -1,65 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-// This function generates all n bit Gray codes and prints the generated codes
-void generateGrayCode(int n)
-{
-    // Base case
-    if (n <= 0)
-        return;
+int main() {
+    int n = 6;
 
-    // 'arr' will store all generated codes
-    char **arr = (char **)malloc(sizeof(char *) * (1 << n));
-    for (int i = 0; i < (1 << n); i++)
-    {
-        arr[i] = (char *)malloc(sizeof(char) * (n + 1));
+    for (int i = 0; i < (1 << n); i++) {
+        int grayCode = i ^ (i >> 1);
+
+        for (int j = n - 1; j >= 0; j--) {
+            printf("%d", (grayCode >> j) & 1);
+        }
+        printf("\n");
     }
 
-    // Start with one-bit pattern
-    strcpy(arr[0], "0");
-    strcpy(arr[1], "1");
-
-    // Every iteration of this loop generates 2*i codes from previously
-    // generated i codes.
-    int i, j;
-    for (i = 2; i < (1 << n); i = i << 1)
-    {
-        // Enter the previously generated codes again in arr[] in reverse
-        // order. Now arr[] has double the number of codes.
-        for (j = i - 1; j >= 0; j--)
-        {
-            strcpy(arr[i + j], arr[j]);
-        }
-
-        // Append '0' to the first half
-        for (j = 0; j < i; j++)
-        {
-            strcat(arr[j], "0");
-        }
-
-        // Append '1' to the second half
-        for (j = i; j < 2 * i; j++)
-        {
-            strcat(arr[j], "1");
-        }
-    }
-
-    // Print contents of arr[]
-    for (i = 0; i < (1 << n); i++)
-    {
-        printf("%s\n", arr[i]);
-        free(arr[i]);
-    }
-
-    // Free the memory allocated for arr
-    free(arr);
-}
-
-// Driver program to test the generateGrayCode function
-int main()
-{
-    int n=6;
-    generateGrayCode(n);
     return 0;
 }
