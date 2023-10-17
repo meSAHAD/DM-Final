e5e1b0142a96b540b5d1f22ac3f1b9fa67364171
Md. Sadekur Rahman Sahad <sahad.cse8.bu@gmail.com>
2023-10-17 22:26:29 +0600

    Update 11.cpp

--- DM Final/11.cpp
+++ DM Final/11.cpp
@@ -1,68 +1,42 @@
 #include <stdio.h>
 
-void findLongestSubsequences(int arr[], int n)
-{
-    int lis[n]; // Longest Increasing Subsequence
-    int lds[n]; // Longest Decreasing Subsequence
+#define N 1000
 
-    // Initialize both arrays with 1 as the minimum length for any subsequence
-    for (int i = 0; i < n; i++)
-    {
-        lis[i] = lds[i] = 1;
-    }
+int n, arr[N], dp[N];
 
-    // Calculate the longest increasing subsequence
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-            {
-                lis[i] = lis[j] + 1;
-            }
+int lis(int i) {
+    if (dp[i] != -1) return dp[i];
+    int ans = 1;
+    for (int j = 0; j < i; j++) {
+        if (arr[j] < arr[i]) {
+            ans = (ans > (lis(j) + 1)) ? ans : (lis(j) + 1);
         }
     }
-
-    // Calculate the longest decreasing subsequence
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = n - 1; j > i; j--)
-        {
-            if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
-            {
-                lds[i] = lds[j] + 1;
-            }
-        }
-    }
-
-    // Find the maximum length of increasing and decreasing subsequences
-    int maxLIS = lis[0];
-    int maxLDS = lds[0];
-
-    for (int i = 1; i < n; i++)
-    {
-        if (lis[i] > maxLIS)
-        {
-            maxLIS = lis[i];
-        }
-        if (lds[i] > maxLDS)
-        {
-            maxLDS = lds[i];
-        }
-    }
-
-    printf("Longest Increasing Subsequence: %d\n", maxLIS);
-    printf("Longest Decreasing Subsequence: %d\n", maxLDS);
+    return dp[i] = ans;
 }
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)  scanf("%d", &arr[i]);
+int lds(int i) {
+    if (dp[i] != -1) return dp[i];
+    int ans = 1;
+    for (int j = 0; j < i; j++) {
+        if (arr[j] > arr[i]) {
+            ans = (ans > (lds(j) + 1)) ? ans : (lds(j) + 1);
+        }
+    }
+    return dp[i] = ans;
+}
 
-    findLongestSubsequences(arr, n);
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        dp[i] = -1;
+    }
+
+    printf("Longest Increasing Subsequence: %d\n", lis(n - 1));
+
+    for (int i = 0; i < n; i++) dp[i] = -1;
+    printf("Longest Decreasing Subsequence: %d\n", lds(n - 1));
 
     return 0;
 }
